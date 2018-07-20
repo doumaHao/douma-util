48ba766bab31a6de1f28ec728e1ce449b230a783
haoxijun <809103454@qq.cm>
2018-07-20 22:45:23 +0800

    开发工具类

--- src/main/java/com/douma/util/json/JsonUtil.java
+++ src/main/java/com/douma/util/json/JsonUtil.java
@@ -1,7 +1,8 @@
 package com.douma.util.json;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.alibaba.fastjson.JSON;
+
+import java.util.Map;
 
 /**
  * Copyright (c) 2017. douma.cn, All Rights Reserved
@@ -12,6 +13,37 @@ import org.slf4j.LoggerFactory;
  */
 public class JsonUtil {
 
-    private final static Logger logger = LoggerFactory.getLogger(JsonUtil.class);
+    /**
+     * Objec转json
+     * @param object
+     * @return
+     */
+    public static String toJson(Object object){
+        String json = null;
+        if(object == null){
+            return json;
+        }
+        return JSON.toJSONString(object);
+    }
+
+    /**
+     * json转Object
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T toObject(String json, Class<T> clazz){
+        return JSON.parseObject(json, clazz);
+    }
+
+    /**
+     * json转Map
+     * @param json
+     * @return
+     */
+    public static Map<String, Object> toMap(String json){
+        return JSON.parseObject(json, Map.class);
+    }
 
 }
